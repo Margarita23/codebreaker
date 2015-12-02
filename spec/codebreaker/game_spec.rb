1885705482d025ca4f0486cde46541d708a3243e
Margarita <margarita.korkina@mail.ru>
2015-12-03 01:07:15 +0200

    alright

--- spec/codebreaker/game_spec.rb
+++ spec/codebreaker/game_spec.rb
@@ -39,8 +39,16 @@ module Codebreaker
       end
       
       it "get 4 numbers from user" do
-        @submit_code = @game.submit_code([1,1,1,6])
-        expect(@submit_code.size).to eq(@secret_code.size)
+        @game.instance_variable_set(:@secret_code,[1,1,1,4])
+        @submit_code = @game.submit_code([1,4,1,1])
+        result = @game.check_submit_code
+        expect(result).to eq(["+","+","-","-"])
+      end
+      
+      it "get 4 numbers from user" do
+        @game.instance_variable_set(:@secret_code,[1,1,1,4])
+        submit_code = @game.submit_code([1,1,1,6])
+        expect(submit_code.size).to eq(@secret_code.size)
       end
       it "should lose one attempt" do
         expect{@game.submit_code([1,2,3,3])}.to change{@game.instance_variable_get(:@attempts)}.by(-1)
@@ -75,80 +83,52 @@ module Codebreaker
           expect(result).to eq([])
         end
       end
-      describe "a number match:" do
-        it "get new array from secret code without numbers that have matchers by values and indeces" do
-          @game.submit_code([1,2,6,5])
-          @game.check_submit_code
-          no_plus = @game.instance_variable_get(:@without_plus_sec)
-          expect(no_plus).to eq([3,4])
-        end
-        it "get new array from submit code without numbers that have matchers by values and indeces" do
-          @game.submit_code([1,2,6,5])
-          @game.check_submit_code
-          no_plus = @game.instance_variable_get(:@without_plus_sub)
-          expect(no_plus).to eq([6,5])
-        end
-      end
-      describe "after get minuses" do
         
-        
-      it "get 4 numbers from user" do
-        @game.instance_variable_set(:@secret_code,[3,4,4,4])
-        @submit_code = @game.submit_code([4,3,3,3])
-        res = @game.check_submit_code
-        expect(res).to eq(["-","-"])
-      end
-      
-        it "get 2 minuses" do
-          @game.submit_code([1,3,2,4])
-          res = @game.check_submit_code
-          expect(res).to eq(["+","+","-"])
+        describe "get the right results in the comparison of the alleged code and secret code" do
+          
+          it "get 1 plus in result" do
+            @game.instance_variable_set(:@secret_code,[1,1,1,3])
+            @game.submit_code([3,3,3,3])
+            @result = @game.check_submit_code
+            expect(@result).to eq(["+"])
+          end
+          it "get 1 plus and 1 minus in result" do
+            @game.instance_variable_set(:@secret_code,[5,1,1,3])
+            @game.submit_code([6,5,1,5])
+            result = @game.check_submit_code
+            expect(result).to eq(["+","-"])
+          end
+          it "get 2 pluses and 1 minus in result" do
+            @game.instance_variable_set(:@secret_code,[5,1,1,3])
+            @game.instance_variable_set(:@guess,[1,2,1,3])
+            result = @game.check_submit_code
+            expect(result).to eq(["+","+","-"])
+          end
+          it "get 4 minuses in result" do
+            @game.instance_variable_set(:@secret_code,[1,2,2,1])
+            @game.instance_variable_set(:@guess,[2,1,1,2])
+            result = @game.check_submit_code
+            expect(result).to eq(["-","-","-","-"])
+          end
+          it "get 3 pluses in result" do
+            @game.instance_variable_set(:@secret_code,[1,1,3,1])
+            @game.instance_variable_set(:@guess,[1,1,2,1])
+            result = @game.check_submit_code
+            expect(result).to eq(["+","+","+"])
+          end
+          it "get 2 minuses in result" do
+            @game.instance_variable_set(:@secret_code,[2,4,4,2])
+            @game.submit_code([1,2,3,4])
+            result = @game.check_submit_code
+            expect(result).to eq(["-","-"])
+          end
+          it "get 2 plus and 1 minus in result" do
+            @game.instance_variable_set(:@secret_code,[3,1,1,6])
+            @game.submit_code([3,1,6,1])
+            result = @game.check_submit_code
+            expect(result).to eq(["+","+","-","-"])
+          end
         end
-        it "get 2 minus in result" do
-          @game.submit_code([3,3,1,1])
-          res = @game.check_submit_code
-          expect(res).to eq(["-","-"])
-        end
-        it "get 1 minuses" do
-          @game.submit_code([5,4,5,6])
-          res = @game.check_submit_code
-          expect(res).to eq(["-"])
-        end
-      end
-      describe "get the right results in the comparison of the alleged code and secret code" do
-        it "get 1 plus in result" do
-          @game.submit_code([3,3,3,3])
-          result = @game.check_submit_code
-          expect(result).to eq(["+"])
-        end
-        it "get 1 minus in result" do
-          @game.submit_code([6,5,1,5])
-          result = @game.check_submit_code
-          expect(result).to eq(["-"])
-        end
-        it "get 1 plus and 1 minus in result" do
-          @game.submit_code([6,2,1,5])
-          result = @game.check_submit_code
-          expect(result).to eq(["+","-"])
-        end
-        it "get 1 plus and 1 minus in result" do
-          @game.submit_code([6,2,1,1])
-          result = @game.check_submit_code
-          expect(result).to eq(["+","-"])
-        end
-        it "get 2 plus and 1 minus in result" do
-          @game.submit_code([6,2,1,4])
-          result = @game.check_submit_code
-          expect(result).to eq(["+","+","-"])
-        end
-        it "get 2 plus and 1 minus in result" do
-          @game.instance_variable_set(:@secret_code,[2,4,4,2])
-          @game.submit_code([1,2,3,4])
-          result = @game.check_submit_code
-          expect(result).to eq(["-","-"])
-        end
-
-      end
     end
     
     describe "#Code-breaker wins game" do
@@ -190,7 +170,6 @@ module Codebreaker
           expect(@game.instance_variable_get(:@attempts)).to eq(12)
           end
       it "may say you lose" do
-        @secret_code = @game.start(3)
           @game.instance_variable_set(:@attempts, 0)
         expect(@game.loss?).to be_truthy
       end
